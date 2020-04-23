378f1a8749f52ea767019b83d1a2c5fb7e279b90
Guy Attali <guyattali97@gmail.com>
2020-04-23 14:38:26 +0300

    Create DecoratedProcedure father class and fixed type comprasion

--- policies/base.py
+++ policies/base.py
@@ -1,38 +1,22 @@
-from abc import abstractmethod
+import abc
 
 from procedures.base import SiteProcedure, PersonProcedure
 
 
-class DecoratedPersonProcedure(PersonProcedure):
-    def __init__(self, procedure: PersonProcedure):
-        self.decorated_procedure: PersonProcedure = procedure
-
-    @abstractmethod
-    def should_apply(self, person):
-        pass
-
-    @abstractmethod
-    def apply(self, person):
-        pass
+class _DecoratedProcedure:
+    def __init__(self, procedure):
+        self.decorated_procedure = procedure
 
     def is_type(self, instance_type):
         return self.decorated_procedure.is_type(instance_type)
 
 
-class DecoratedSiteProcedure(SiteProcedure):
-    def __init__(self, procedure: SiteProcedure):
-        self.decorated_procedure: SiteProcedure = procedure
+class DecoratedPersonProcedure(PersonProcedure, _DecoratedProcedure, abc.ABC):
+    pass
 
-    @abstractmethod
-    def should_apply(self, site):
-        pass
 
-    @abstractmethod
-    def apply(self, site):
-        pass
-
-    def is_type(self, instance_type):
-        return self.decorated_procedure.is_type(instance_type)
+class DecoratedSiteProcedure(SiteProcedure, _DecoratedProcedure, abc.ABC):
+    pass
 
 
 class Policy:
@@ -45,3 +29,6 @@ class Policy:
 
     def world_posttick(self):
         pass
+
+
+__all__ = [DecoratedPersonProcedure, DecoratedSiteProcedure, Policy]
