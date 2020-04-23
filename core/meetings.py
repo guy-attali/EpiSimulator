006680f3662f2b4cb109bff2454d2fcc1b1ab53c
Shir Goldfarb <shir.goldfarb@gmail.com>
2020-04-23 18:23:47 +0300

    Adapting meetings to existing code
    and create meetings in site.tick()

--- core/meetings.py
+++ core/meetings.py
@@ -1,24 +1,27 @@
 from core.world import world
+from core.person import Person
+from traits.base import PERSON_TRAIT_TYPE
 
 
 class Meeting:
     """
     Represents a single Meeting between two or more people
     """
-    def __init__(self, person1, person2, site):
+    def __init__(self, person1: Person, person2: Person, site):
         self._time = world.current_ts
         self._people_involved = [person1, person2]
         self._location = site
 
     def __repr__(self):
         string = "Meeting(Location: {0} \n \t person1: {1} \n \t person2: {2} \n \t meeting time: {3} \n \t is meeting infected: {4}".format(
-            self._location, str(self._people_involved[0]), str(self._people_involved[1]), self._time, self.is_meeting_infected()
+            self._location, str(self._people_involved[0].uuid), str(self._people_involved[1].uuid),
+            self._time, self.is_meeting_infected()
         )
         return string
 
     def __str__(self):
         string = "Meeting(Location: {0} \n person1: {1} \n person2: {2} \n meeting time: {3} \n is meeting infected: {4}".format(
-            self._location, str(self._people_involved[0]), str(self._people_involved[1]), self._time,
+            self._location, str(self._people_involved[0].uuid), str(self._people_involved[1].uuid), self._time,
             self.is_meeting_infected()
         )
         return string
@@ -35,6 +38,5 @@ class Meeting:
         checks if one of the people in the meeting is infected.
         :return boolean
         """
-        return self._people_involved[1].is_infected or self._people_involved[0].is_infected
-
-
+        return self._people_involved[1].traits[PERSON_TRAIT_TYPE.INFECTED] or\
+               self._people_involved[0].traits[PERSON_TRAIT_TYPE.INFECTED]
