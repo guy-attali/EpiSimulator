8552f9dc37bf692ca8ae09333d956ad677c6cb69
Lior Deutsch <sliorde@gmail.com>
2020-04-25 07:33:02 +0300

    added metrics to main script

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from core.world import world
 from scenarios.scenario1 import setup_world
+from metrics import MetricManager
+
+metrics = MetricManager()
+metrics_interval = 12
 
 def main():
     setup_world()
-    world.tick()
+
+    while True:
+        if world.current % metrics_interval == 0:
+            metrics.show()
+
+        world.tick()
 
 if __name__ == '__main__':
     main()
